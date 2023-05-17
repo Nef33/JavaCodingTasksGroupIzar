76b2cf6f568a2f543ced0733d0265b0efd4e396a
Nefi33 <nefisekyrk@gmail.com>
2023-05-17 15:21:03 -0500

    First task is complete

---
+++ src/OddEven.java
@@ -0,0 +1,17 @@
+public class OddEven {
+
+    public static void main(String[] args) {
+
+
+     int number=0;
+
+     if(number%2==1) {
+         System.out.println(number + " is odd number");
+     }
+         else{
+         System.out.println(number+" is even number");
+         }
+
+    }
+
+}
