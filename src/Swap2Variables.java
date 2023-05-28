a03a5622eb1af4327d45fd85e4d0c49f6fc467f3
Nefi33 <nefisekyrk@gmail.com>
2023-05-28 15:25:07 -0500

    2 of Java Task2 completed

---
+++ src/Swap2Variables.java
@@ -0,0 +1,16 @@
+public class Swap2Variables {
+    public static void main(String[] args) {
+
+
+    int a=-9;
+    int b=-15;
+
+    b= b-a;
+    a=a+b;
+
+    b=a-b;
+
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+
+}}
